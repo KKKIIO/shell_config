e62e0b5d6d28aa9eb1f94dd82c004528143b127e
kkkiio <xc130d210@outlook.com>
2020-02-18 10:22:53 +0800

    feat(zsh, style): init

--- init.sh
+++ init.sh
@@ -1,2 +1,7 @@
-eval "$(lua ~/.local/shell_config/z.lua  --init bash once enhanced)"
+if [ -n "$ZSH_VERSION" ]; then
+    shell="zsh"
+else 
+    shell="bash"
+fi
 
+eval "$(lua ~/.local/shell_config/z.lua  --init $shell once enhanced)"
