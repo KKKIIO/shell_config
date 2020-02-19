e07b078c6665c31c861940dad24acde1abfc35dc
kkkiio <xc130d210@outlook.com>
2020-02-19 23:21:11 +0800

    fix(bootstrap): fix bug

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -3,7 +3,7 @@ cd ~/.local
 git clone https://github.com/KKKIIO/shell_config.git 
 cd shell_config
 Invoke-WebRequest https://raw.githubusercontent.com/skywind3000/z.lua/master/z.lua -OutFile z.lua
-$source_init += '$fileContents = [string]::join([environment]::newline, (get-content -path ~/.local/shell_config/init.ps1))'
-$source_init += "`n"
-$source_init += 'invoke-expression $fileContents'
-$source_init | Out-File -FilePath $profile.CurrentUserAllHosts
+@'
+$fileContents = [string]::join([environment]::newline, (get-content -path ~/.local/shell_config/init.ps1))
+invoke-expression $fileContents
+'@ | Out-File -FilePath $profile.CurrentUserAllHosts
